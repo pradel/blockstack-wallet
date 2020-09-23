229bee69312a8039d5abfb9fb09ce669350e94e2
Léo Pradel <pradel.leo@gmail.com>
2020-09-23 10:44:11 +0200

    feat: save theme in async storage (#19)

---
+++ src/components/StatusBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { useTheme } from '../context/ThemeContext';
+
+/**
+ * Status bar for the app reacting to the theme change.
+ * Will set:
+ * - color of the text
+ * - color of the background (android only)
+ */
+export const StatusBar = () => {
+  const { theme } = useTheme();
+
+  return (
+    <ExpoStatusBar
+      animated
+      style={theme === 'light' ? 'dark' : 'light'}
+      backgroundColor={theme === 'light' ? undefined : '#191D33'}
+    />
+  );
+};
