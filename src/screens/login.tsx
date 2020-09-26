2ed2d5f0337b708b3589cdb8ce7232ed4d8e29cb
pradel <pradel.leo@gmail.com>
2020-09-26 13:13:51 +0200

    chore: use blockstack deriveStxAddressChain

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -6,11 +6,8 @@ import * as LocalAuthentication from 'expo-local-authentication';
 import * as SecureStore from 'expo-secure-store';
 import { useNavigation } from '@react-navigation/native';
 import { ChainID } from '@blockstack/stacks-transactions';
-import {
-  getStorageKeyPk,
-  getRootKeychainFromMnemonic,
-  deriveStxAddressChain,
-} from '../utils';
+import { deriveStxAddressChain } from '@blockstack/keychain';
+import { getStorageKeyPk, getRootKeychainFromMnemonic } from '../utils';
 import { useAuth } from '../context/AuthContext';
 import { useAppConfig } from '../context/AppConfigContext';
 
