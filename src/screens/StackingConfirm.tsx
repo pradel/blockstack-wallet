904af1e23630b852c0d8a2aff30afd7d6632eaf7
pradel <pradel.leo@gmail.com>
2020-10-20 22:58:25 +0200

    show stacking infos

--- src/screens/StackingConfirm.tsx
+++ src/screens/StackingConfirm.tsx
@@ -6,7 +6,8 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import {
   CoreNodePoxResponse,
-  AddressBalanceResponse,
+  CoreNodeInfoResponse,
+  NetworkBlockTimesResponse,
 } from '@stacks/blockchain-api-client';
 import {
   bufferCV,
@@ -16,10 +17,11 @@ import {
   tupleCV,
   uintCV,
 } from '@blockstack/stacks-transactions';
+import { format } from 'date-fns';
 import { AppbarHeader } from '../components/AppbarHeader';
 import { AppbarContent } from '../components/AppBarContent';
 import { Button } from '../components/Button';
-import { microToStacks, stacksToMicro } from '../utils';
+import { microToStacks } from '../utils';
 import { stacksClientInfo, stacksClientSmartContracts } from '../stacksClient';
 import { RootStackParamList } from '../types/router';
 import { useAuth } from '../context/AuthContext';
@@ -38,24 +40,53 @@ export const StackingConfirmScreen = () => {
   const route = useRoute<StackingConfirmScreenRouteProp>();
   const auth = useAuth();
   const [isEligible, setIsEligible] = useState<true | string>();
+  const [stackingInfos, setStackingInfos] = useState<{
+    nextCycleStartingAt: Date;
+    unlockingAt: Date;
+  }>();
   const [stacksInfo, setStacksInfo] = useState<{
     poxInfo: CoreNodePoxResponse;
-    accountBalance: AddressBalanceResponse;
+    coreInfo: CoreNodeInfoResponse;
+    blockTimeInfo: NetworkBlockTimesResponse;
   }>();
+
+  useEffect(() => {
+    const fetchPoxInfo = async () => {
+      try {
+        const poxInfo = await stacksClientInfo.getPoxInfo();
+        const coreInfo = await stacksClientInfo.getCoreApiInfo();
+        const blockTimeInfo = await stacksClientInfo.getNetworkBlockTimes();
+        setStacksInfo({
+          poxInfo,
+          coreInfo,
+          blockTimeInfo,
+        });
+      } catch (error) {
+        // TODO show error to the user
+        console.error(error);
+      }
+    };
+
+    fetchPoxInfo();
+  }, [setStacksInfo]);
 
   // TODO
   const numberOfCycles = 1;
 
   useEffect(() => {
     const fetchIsUserEligible = async () => {
+      if (!stacksInfo) return;
+
       try {
-        const poxInfo = await stacksClientInfo.getPoxInfo();
         // derive bitcoin address from Stacks account and convert into required format
         const hashbytes = bufferCV(
           global.Buffer.from(route.params.bitcoinAddress, 'hex')
         );
         const version = bufferCV(global.Buffer.from('01', 'hex'));
-        const [contractAddress, contractName] = poxInfo.contract_id.split('.');
+        const [
+          contractAddress,
+          contractName,
+        ] = stacksInfo.poxInfo.contract_id.split('.');
 
         // read-only contract call
         const isEligible = await stacksClientSmartContracts.callReadOnlyFunction(
@@ -76,9 +107,9 @@ export const StackingConfirmScreen = () => {
                   'hex'
                 )}`,
                 // explicilty check eligibility for next cycle
-                `0x${serializeCV(uintCV(poxInfo.reward_cycle_id)).toString(
-                  'hex'
-                )}`,
+                `0x${serializeCV(
+                  uintCV(stacksInfo.poxInfo.reward_cycle_id)
+                ).toString('hex')}`,
                 `0x${serializeCV(uintCV(numberOfCycles)).toString('hex')}`,
               ],
             },
@@ -95,7 +126,6 @@ export const StackingConfirmScreen = () => {
         );
 
         if (response.startsWith(`(err `)) {
-          // TODO convert error code into human readable message
           setIsEligible(response);
           return;
         }
@@ -105,10 +135,39 @@ export const StackingConfirmScreen = () => {
         // TODO show error to the user
         console.error(error);
       }
+
+      // how long (in seconds) is a Stacking cycle?
+      const cycleDuration =
+        stacksInfo.poxInfo.reward_cycle_length *
+        stacksInfo.blockTimeInfo.testnet.target_block_time;
+
+      // how much time is left (in seconds) until the next cycle begins?
+      const secondsToNextCycle =
+        (stacksInfo.poxInfo.reward_cycle_length -
+          ((stacksInfo.coreInfo.burn_block_height -
+            stacksInfo.poxInfo.first_burnchain_block_height) %
+            stacksInfo.poxInfo.reward_cycle_length)) *
+        stacksInfo.blockTimeInfo.testnet.target_block_time;
+
+      // the actual datetime of the next cycle start
+      const nextCycleStartingAt = new Date();
+      nextCycleStartingAt.setSeconds(
+        nextCycleStartingAt.getSeconds() + secondsToNextCycle
+      );
+
+      const unlockingAt = new Date(nextCycleStartingAt);
+      unlockingAt.setSeconds(
+        unlockingAt.getSeconds() +
+          stacksInfo.poxInfo.reward_cycle_length *
+            numberOfCycles *
+            stacksInfo.blockTimeInfo.testnet.target_block_time
+      );
+
+      setStackingInfos({ nextCycleStartingAt, unlockingAt });
     };
 
     fetchIsUserEligible();
-  }, [setStacksInfo]);
+  }, [stacksInfo]);
 
   const handleConfirm = () => {
     // TODO
@@ -117,6 +176,8 @@ export const StackingConfirmScreen = () => {
   // TODO check amount entered does not exceed current balance
 
   const canConfirm = isEligible === true;
+
+  const showLoading = isEligible === undefined || !stackingInfos;
 
   return (
     <View style={styles.container}>
@@ -132,7 +193,7 @@ export const StackingConfirmScreen = () => {
           />
         </AppbarHeader>
 
-        {isEligible === undefined ? (
+        {showLoading ? (
           <View>
             <ActivityIndicator animating={true} />
           </View>
@@ -145,14 +206,27 @@ export const StackingConfirmScreen = () => {
           </View>
         ) : null}
 
-        {isEligible === true ? (
+        {isEligible === true && stackingInfos ? (
           <View>
             <List.Item
               title="Amount to lock"
               description={`${microToStacks(route.params.amountInMicro)} STX`}
             />
             <List.Item title="Number of cycles" description={numberOfCycles} />
-            <List.Item title="Start date" description={'TODO'} />
+            <List.Item
+              title="Start date"
+              description={format(
+                stackingInfos.nextCycleStartingAt,
+                'HH:mm dd MMMM yyyy'
+              )}
+            />
+            <List.Item
+              title="Unlocking date"
+              description={format(
+                stackingInfos.unlockingAt,
+                'HH:mm dd MMMM yyyy'
+              )}
+            />
             <List.Item
               title="BTC address"
               description={route.params.bitcoinAddress}
