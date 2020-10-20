175c3a29b9433ca947eca4fcf83244e54992c1cb
pradel <pradel.leo@gmail.com>
2020-10-20 19:17:03 +0200

    stacking image

--- src/screens/Stacking.tsx
+++ src/screens/Stacking.tsx
@@ -8,11 +8,15 @@ import { AppbarHeader } from '../components/AppbarHeader';
 import { AppbarContent } from '../components/AppBarContent';
 import { Button } from '../components/Button';
 import { RootStackParamList } from '../types/router';
+import { UndrawSavings } from '../images/UndrawSavings';
 
 type StackingScreenNavigationProp = StackNavigationProp<RootStackParamList>;
 
 export const StackingScreen = () => {
   const navigation = useNavigation<StackingScreenNavigationProp>();
+
+  // TODO only show this if user is not already stacking
+  // TODO if user is stacking show stacking dashboard with metrics
 
   return (
     <View style={styles.container}>
@@ -23,9 +27,12 @@ export const StackingScreen = () => {
         />
       </AppbarHeader>
 
-      <Text>TODO how it works button that open a new link</Text>
-      <Text>TODO nice illustration</Text>
-      <Text>TODO only show this if user is not already stacking</Text>
+      <View style={styles.imageContainer}>
+        <UndrawSavings height={200} />
+
+        <Text>TODO some basic explanations</Text>
+        <Text>TODO how it works button that open a new link</Text>
+      </View>
 
       <View style={styles.buttonsContainer}>
         <Button
@@ -43,6 +50,10 @@ const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
     flex: 1,
+    justifyContent: 'space-between',
+  },
+  imageContainer: {
+    alignItems: 'center',
   },
   buttonsContainer: {
     padding: 16,
