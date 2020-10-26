50810d42a1d08964c549a92dafca617a433d6344
pradel <pradel.leo@gmail.com>
2020-10-26 12:31:06 +0100

    feat: setup sentry for react-native

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import './src/globals';
 import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
-
+import * as Sentry from '@sentry/react-native';
 import App from './App';
+
+Sentry.init({
+  enabled: process.env.NODE_ENV === 'production',
+  dsn:
+    'https://7f620092734740a0a349e69961076b58@o72928.ingest.sentry.io/5493197',
+  enableAutoSessionTracking: true,
+});
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
