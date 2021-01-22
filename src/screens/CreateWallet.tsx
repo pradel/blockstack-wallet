fae72b0e101d622c9bce0690018179cfee3bf4e5
pradel <pradel.leo@gmail.com>
2021-01-22 15:06:19 +0100

    add copy to create wallet

--- src/screens/CreateWallet.tsx
+++ src/screens/CreateWallet.tsx
@@ -7,6 +7,7 @@ import {
   Paragraph,
   ActivityIndicator,
   Appbar,
+  Snackbar,
 } from 'react-native-paper';
 import { ChainID } from '@stacks/transactions';
 import { deriveStxAddressChain } from '@stacks/keychain';
@@ -15,6 +16,7 @@ import * as LocalAuthentication from 'expo-local-authentication';
 import { BIP32Interface } from 'bitcoinjs-lib';
 import * as Sentry from '@sentry/react-native';
 import { useNavigation } from '@react-navigation/native';
+import Clipboard from '@react-native-community/clipboard';
 import { useAuth } from '../context/AuthContext';
 import { getStorageKeyPk, generateMnemonicRootKeychain } from '../utils';
 import { Button } from '../components/Button';
@@ -27,6 +29,7 @@ export const CreateWalletScreen = () => {
     rootNode: BIP32Interface;
     plaintextMnemonic: string;
   }>();
+  const [snackbarVisible, setSnackbarVisible] = useState(false);
 
   useEffect(() => {
     // when the screen is mounted we generate a new private key
@@ -37,6 +40,13 @@ export const CreateWalletScreen = () => {
 
     generateMnemonic();
   }, []);
+
+  const handleCopyClipboard = () => {
+    if (mnemonic) {
+      Clipboard.setString(mnemonic.plaintextMnemonic);
+      setSnackbarVisible(true);
+    }
+  };
 
   const handleCreateNewWallet = async () => {
     if (!mnemonic) {
@@ -68,30 +78,35 @@ export const CreateWalletScreen = () => {
 
   return (
     <View style={styles.container}>
-      <View>
-        <AppbarHeader>
-          <Appbar.BackAction onPress={() => navigation.goBack()} />
-        </AppbarHeader>
+      <AppbarHeader>
+        <Appbar.BackAction onPress={() => navigation.goBack()} />
+      </AppbarHeader>
 
-        <Title style={styles.title}>Write down your mnemonic</Title>
-        <Paragraph style={styles.paragraph}>
-          These words are the keys to access your blockstack wallet. Keep it in
-          a safe place and do not share it with anyone.
-        </Paragraph>
+      <View style={styles.contentContainer}>
+        <View>
+          <Title style={styles.title}>Write down your mnemonic</Title>
+          <Paragraph>
+            These words are the keys to access your blockstack wallet. Keep it
+            in a safe place and do not share it with anyone.
+          </Paragraph>
 
-        {mnemonic ? (
-          <Surface style={styles.wordContainer}>
-            {mnemonic.plaintextMnemonic.split(' ').map((word, index) => (
-              <Paragraph key={index} style={styles.text}>
-                {word}
-              </Paragraph>
-            ))}
-          </Surface>
-        ) : (
-          <View style={styles.loadingContainer}>
-            <ActivityIndicator animating={true} />
-          </View>
-        )}
+          {mnemonic ? (
+            <>
+              <Surface style={styles.wordContainer}>
+                {mnemonic.plaintextMnemonic.split(' ').map((word, index) => (
+                  <Paragraph key={index} style={styles.text}>
+                    {word}
+                  </Paragraph>
+                ))}
+              </Surface>
+              <Button onPress={handleCopyClipboard}>Copy to clipboard</Button>
+            </>
+          ) : (
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator animating={true} />
+            </View>
+          )}
+        </View>
       </View>
 
       <View style={styles.buttonsContainer}>
@@ -103,6 +118,14 @@ export const CreateWalletScreen = () => {
           Save wallet
         </Button>
       </View>
+
+      <Snackbar
+        visible={snackbarVisible}
+        duration={3000}
+        onDismiss={() => setSnackbarVisible(false)}
+      >
+        Copied to clipboard
+      </Snackbar>
     </View>
   );
 };
@@ -111,9 +134,11 @@ const styles = StyleSheet.create({
   container: {
     marginTop: Constants.statusBarHeight,
     flex: 1,
+  },
+  contentContainer: {
+    flex: 1,
     justifyContent: 'space-between',
-    paddingLeft: 16,
-    paddingRight: 16,
+    paddingHorizontal: 16,
   },
   loadingContainer: {
     flex: 1,
@@ -123,10 +148,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 26,
     marginTop: 16,
-    textAlign: 'center',
-  },
-  paragraph: {
-    textAlign: 'center',
   },
   wordContainer: {
     marginTop: 64,
@@ -140,7 +161,7 @@ const styles = StyleSheet.create({
     marginRight: 8,
   },
   buttonsContainer: {
-    paddingTop: 16,
-    paddingBottom: 16,
+    paddingVertical: 16,
+    paddingHorizontal: 16,
   },
 });
