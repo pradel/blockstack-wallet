fd4276cda1092aa421f0aa7a2b249ad944fe35db
pradel <pradel.leo@gmail.com>
2021-01-28 18:25:00 +0100

    Update Dashboard.tsx

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -76,8 +76,6 @@ export const DashboardScreen = () => {
       ...mempoolTransactions.results,
       ...accountTransactions.results,
     ];
-    // TODO remove
-    console.log(mempoolTransactions.results);
     return result;
   });
   const [isRefreshing, setIsRefreshing] = useState(false);
