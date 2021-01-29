16f500a5594137da6422b12e13406cb566b2be6d
pradel <pradel.leo@gmail.com>
2021-01-29 15:21:51 +0100

    chore: use react-query instead of swr

--- App.tsx
+++ App.tsx
@@ -7,11 +7,18 @@ import {
   Inter_500Medium,
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from './src/context/ThemeContext';
 import { ConfigProvider } from './src/context/AppConfigContext';
 import { AuthProvider } from './src/context/AuthContext';
 import { Router } from './src/Router';
 import { StatusBar } from './src/components/StatusBar';
+import { LogBox } from 'react-native';
+
+// for react-query https://github.com/tannerlinsley/react-query/issues/1259#issuecomment-745623606
+LogBox.ignoreLogs(['Setting a timer']);
+
+const queryClient = new QueryClient();
 
 export default () => {
   let [fontsLoaded] = useFonts({
@@ -26,13 +33,15 @@ export default () => {
   }
 
   return (
-    <ThemeProvider>
-      <ConfigProvider>
-        <AuthProvider>
-          <Router />
-          <StatusBar />
-        </AuthProvider>
-      </ConfigProvider>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <ConfigProvider>
+          <AuthProvider>
+            <Router />
+            <StatusBar />
+          </AuthProvider>
+        </ConfigProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
